e773c8fd3145f37d6cc8bf7a441dad13eeb2c4fa
Keanna Salter <keannasalter91@gmail.com>
2020-01-30 22:50:15 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,12 @@ def take_a_number(katz_deli, name)
   katz_deli <<name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in lne."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+  puts " There is nobody waiting to be served!"
+else
+  katz_deli.shift
+  puts "Currently serving #{katz_deli.shift}"
+  end
+end
